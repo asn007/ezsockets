bc6421ec7b716537c0e9fc1499cd4ea5d26d58f4
Andrey Sinitsyn <andrey.sin98@gmail.com>
2024-06-11 22:42:20 +0300

    feat: delay reconnect on socket death

--- src/client.rs
+++ src/client.rs
@@ -595,6 +595,7 @@ impl<E: ClientExt, C: ClientConnector> ClientActor<E, C> {
                 match self.client.on_disconnect().await? {
                     ClientCloseMode::Reconnect => {
                         std::mem::drop(socket);
+                        sleep(self.config.reconnect_interval).await;
                         let Some(socket) = client_connect(
                             self.config.max_reconnect_attempts,
                             &self.config,
